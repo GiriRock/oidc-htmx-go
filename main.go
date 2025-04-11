c40f3e83a1756754d3b6dda9fe9c2104ae5c8cbc
Giridharan Mohan <giridharanrock@gmail.com>
2025-04-11 15:55:25 +0400

    initial commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"html/template"
+	"io"
+)
+
+type Templates struct {
+	templates *template.Template
+}
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func newTemplate() *Templates {
+	return &Templates{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+}
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Renderer = newTemplate()
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(200, "index", nil)
+	})
+	e.Logger.Fatal(e.Start(":42069"))
+}
